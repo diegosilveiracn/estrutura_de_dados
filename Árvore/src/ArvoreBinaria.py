723d93ab4c8247f5f6d3465aaca7c5110bd9d84d
diegosilveiracn <diegoscnascimento@gmail.com>
2016-12-19 17:47:14 -0300

    Incluindo o método para excluir um nó em uma árvore binária.

--- Árvore/src/ArvoreBinaria.py
+++ Árvore/src/ArvoreBinaria.py
@@ -25,7 +25,64 @@ class ArvoreBinaria:
                 no.ramoDireito = No(valor)
             else:
                 self.__inserir(valor, no.ramoDireito)
-
+    
+    def remover(self, valor):
+        self.__remover(None, self.raiz, valor)
+    
+    def __remover(self, no_pai, no, valor):
+        if no != None:
+            if no.valor != valor:
+                if no.valor > valor:
+                    self.__remover(self, no, no.ramoEsquerdo, valor)
+                else:
+                    self.__remover(self, no, no.ramoDireito, valor)
+            else:
+                # Remover nó folha
+                if no.ramoEsquerdo == None and no.ramoDireito == None:
+                    if no.valor == self.raiz.valor:
+                        self.raiz = None
+                    elif no_pai.ramoDireito.valor == no.valor:
+                        no_pai.ramoDireito = None
+                    else:
+                        no_pai.ramoEsquerdo = None
+                # Remover nó com uma subárvore
+                elif no.ramoEsquerdo == None or no.ramoDireito == None:
+                    if no.ramoEsquerdo == None:
+                        if no.valor == self.raiz.valor:
+                            self.raiz = no.ramoDireito
+                        elif no_pai.ramoEsquerdo.valor == no.valor:
+                            no_pai.ramoEsquerdo = no.ramoDireito
+                        else:
+                            no_pai.ramoDireito = no.ramoDireito
+                    else:
+                        if no.valor == self.raiz.valor:
+                            self.raiz = no.ramoEsquerdo
+                        elif no_pai.ramoEsquerdo == no.valor:
+                            no_pai.ramoEsquerdo = no.ramoEsquerdo
+                        else: 
+                            no_pai.ramoDireito = no.ramoEsquerdo
+                # Remover nó com duas subárvore
+                else:
+                    nodo = no.ramoEsquerdo
+                    anterior = no
+                    
+                    while nodo.ramoDireito != None:
+                        anterior = nodo
+                        nodo = nodo.ramoDireito
+                    
+                    if nodo.ramoEsquerdo == None:
+                        anterior.ramoDireito = None # É uma folha
+                    else:
+                        anterior.ramoDireito = nodo.ramoEsquerdo
+                        
+                    if no.valor == self.raiz.valor:
+                        self.raiz = nodo
+                        nodo.ramoEsquerdo = no.ramoEsquerdo
+                        nodo.ramoDireito = no.ramoDireito
+                    elif no_pai.ramoEsquerdo.valor == no.valor:
+                        no_pai.ramoEsquerdo = nodo
+                    else:
+                        no_pai.ramoDireito = nodo     
                     
 arvore = ArvoreBinaria()
         
