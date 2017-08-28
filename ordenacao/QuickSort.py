031d31cd75f1407829a6a6fcd6704b43e3e3389f
diegosilveiracn <diegoscnascimento@gmail.com>
2017-08-27 23:30:18 -0300

    refatorando a função quicksort

--- ordenacao/QuickSort.py
+++ ordenacao/QuickSort.py
@@ -1,33 +1,30 @@
-def quick_sort(lista):
-    sort(lista, 0, len(lista) - 1)
-
-def sort(lista, index_inicio, index_fim):
-    index_corte = partition(lista, index_inicio, index_fim)
+def quick_sort(lista, index_inicio=None, index_fim=None):
+    if index_inicio == None and index_fim == None:
+        index_inicio = 0
+        index_fim = len(lista) - 1
     
-    if index_inicio < index_corte - 1:
-        sort(lista, index_inicio, index_corte - 1)
-    if index_corte < index_fim:
-        sort(lista, index_corte, index_fim)
-
-def partition(lista, index_inicio, index_fim):
     pivo = lista[(index_inicio + index_fim) // 2]
     i = index_inicio
     j = index_fim
 
-    while i <= j:
+    while i < j:
         while lista[i] < pivo:
             i += 1
 
         while lista[j] > pivo:
             j -= 1
 
-        if i <= j:    
+        if i < j:    
             lista[i], lista[j] = lista[j], lista[i]
-            i += 1
-            j -= 1
+        i += 1
+        j -= 1
 
-    return i
+    if j > index_inicio:
+        quick_sort(lista, index_inicio, j)
+    if i < index_fim:
+        quick_sort(lista, j+1, index_fim)
 
-valores = [5, 8, 9, 2, 1]
+valores = [7,1,3,9,8,4,2,7,4,2,3,5]
 quick_sort(valores)
 print(valores)
+
